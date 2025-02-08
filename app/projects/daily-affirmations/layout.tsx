06ba69e07b93d6c6af4d603afdb46726c4191648
isatimur <isatimur.it@gmail.com>
2025-02-08 12:31:11 +0100

    init v1.0.7 adding real links to first project

--- app/projects/daily-affirmations/layout.tsx
+++ app/projects/daily-affirmations/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "../../globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from '@vercel/analytics/react';
 import Script from 'next/script';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
